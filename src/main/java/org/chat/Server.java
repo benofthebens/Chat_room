c2420fd4f51587a5384ca162bd37844dd973e10c
benjamin whalley <benjaminjwhalley@outlook.com>
2024-04-15 21:51:01 +0100

    feature@Server: added an abstract class for server that can be inherited from

--- src/main/java/org/chat/Server.java
+++ src/main/java/org/chat/Server.java
@@ -2,25 +2,12 @@ package org.chat;
 
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
 
-public class Server {
-    private final ServerSocket serverSocket;
+abstract class Server {
+    protected final ServerSocket serverSocket;
     public Server(int port) throws IOException {
         this.serverSocket = new ServerSocket(port);
     }
-    public void start() throws IOException {
-
-        while(!this.serverSocket.isClosed()){
-            Socket client = serverSocket.accept();
-            ClientHandler clientHandler = new ClientHandler(client);
-            Thread thread = new Thread(clientHandler);
-            thread.start();
-        }
-
-    }
-    public static void main(String[] args) throws IOException {
-        Server server = new Server(3000);
-        server.start();
-    }
+    public abstract void start() throws IOException;
+    public abstract void stop();
 }
