d6932e7e5cadca0c214beb002830cc464c5ab034
benjamin whalley <benjaminjwhalley@outlook.com>
2024-04-15 21:54:46 +0100

    feature@Server: added an inherited class from servers to for calls

---
+++ src/main/java/org/chat/CallServer.java
@@ -0,0 +1,19 @@
+package org.chat;
+
+import java.io.IOException;
+
+public class CallServer extends Server {
+    public CallServer(int port) throws IOException {
+        super(port);
+    }
+
+    @Override
+    public void start() throws IOException {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
