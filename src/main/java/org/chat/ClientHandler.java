701716a456a6ee96e7b024b54bda2e544ee68cf7
benjamin whalley <benjaminjwhalley@outlook.com>
2024-04-15 22:38:44 +0100

    feature@ClientHandler: added an abstract class of ClientHandler.java

--- src/main/java/org/chat/ClientHandler.java
+++ src/main/java/org/chat/ClientHandler.java
@@ -1,5 +1,24 @@
 package org.chat;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public abstract class ClientHandler {
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    protected final Socket client;
+    protected final BufferedReader in;
+    public final BufferedWriter out;
+    public final String username;
+    public ClientHandler(Socket client) throws IOException {
+        this.client = client;
+        this.out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+        this.in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        this.username = in.readLine();
+        clientHandlers.add(this);
+    }
+
+    protected abstract void brodcastMessage(String messageFromClient) throws IOException;
+
 
 }
